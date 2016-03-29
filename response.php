36cee0f86c781549071ff7b9ab63291bbaa55095
Brake-Benji <776080531@qq.com>
2016-03-29 21:25:57 +0800

    修改response文件学习内容，添加笔记

--- response.php
+++ response.php
@@ -1,5 +1,25 @@
 <?php
 
+/*$arr = array(
+	'id' => 1,
+	'name' => 'username',
+	'pwd' => 123456
+
+ );
+
+$data = "输出json数据";
+$newData = iconv('UTF-8', 'GBK', $data);  //次函数用于字符串的编码转换
+echo $newData;
+echo json_encode($newData);*/
+
+/**
+* 按JSON方式输出通信数据（封装）
+* @param integer $code 状态码
+* @param string  $message 提示信息
+* @param array $data 数据
+* return string
+*/
+
 class Response {
 	const JSON = "json";
 	/**
@@ -22,7 +42,8 @@ class Response {
 			'message' => $message,
 			'data' => $data,
 		);
-
+                
+                //$type = self::JSON走这里.地址栏jsonTest.php?format=json这样访问即可
 		if($type == 'json') {
 			self::json($code, $message, $data);
 			exit;
@@ -35,6 +56,25 @@ class Response {
 			// TODO
 		}
 	}
+        
+        /*public static function xml(){
+		//以XML的形式展现
+        header("Content-Type:text/xml");
+
+		//组装字符串的方法
+		$xml = "<?xml vesion='1.0' encoding='UTF-8'?>\n";
+		$xml .= "<root>\n";
+		$xml .= "<code>200</code>\n";
+		$xml .= "<message>数据返回成功</message>\n";
+		$xml .= "<data>\n";
+		$xml .= "<id>1</id>\n";
+		$xml .= "<name>benji</name>\n";
+		$xml .= "</data>\n";
+		$xml .= "</root>\n";
+
+		echo $xml;
+	}*/
+
 	/**
 	* 按json方式输出通信数据
 	* @param integer $code 状态码
@@ -79,7 +119,8 @@ class Response {
 		header("Content-Type:text/xml");
 		$xml = "<?xml version='1.0' encoding='UTF-8'?>\n";
 		$xml .= "<root>\n";
-
+                
+                //这边可以是$result也可以是$data,但是区别很大效果不一样
 		$xml .= self::xmlToEncode($result);
 
 		$xml .= "</root>";
@@ -87,14 +128,16 @@ class Response {
 	}
 
 	public static function xmlToEncode($data) {
-
+                //定义变量存储XML数据
 		$xml = $attr = "";
 		foreach($data as $key => $value) {
 			if(is_numeric($key)) {
 				$attr = " id='{$key}'";
 				$key = "item";
 			}
+                        //加大括号是为了识别标签内容
 			$xml .= "<{$key}{$attr}>";
+                        //$result时是数组，要递归下
 			$xml .= is_array($value) ? self::xmlToEncode($value) : $value;
 			$xml .= "</{$key}>\n";
 		}
